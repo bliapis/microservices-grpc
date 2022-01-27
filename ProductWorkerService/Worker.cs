6ec3d9166025487325910b48636f15cf4e361878
Bruno Liapis <liapisb@gmail.com>
2022-01-27 15:47:46 -0300

    added ProductWorker

---
+++ ProductWorkerService/Worker.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductWorkerService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
+
+        public Worker(ILogger<Worker> logger,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                var taskInterval = _configuration.GetValue<int>("WorkerService:TaskInterval");
+
+                await Task.Delay(taskInterval, stoppingToken);
+            }
+        }
+    }
+}
