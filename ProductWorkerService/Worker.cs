4e3d0c795001f272bf8aa998214c35bb03125929
Bruno Liapis <liapisb@gmail.com>
2022-01-27 15:59:30 -0300

    update worker to add product

--- ProductWorkerService/Worker.cs
+++ ProductWorkerService/Worker.cs
@@ -1,6 +1,9 @@
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProductGrpc.Protos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,11 +26,33 @@ namespace ProductWorkerService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            var taskInterval = _configuration.GetValue<int>("WorkerService:TaskInterval");
+            var serverUrl = _configuration.GetValue<string>("WorkerService:ServerUrl");
+            var productName = _configuration.GetValue<string>("WorkerService:ProductName");
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
 
-                var taskInterval = _configuration.GetValue<int>("WorkerService:TaskInterval");
+                using var channel = GrpcChannel.ForAddress(serverUrl);
+                var client = new ProductProtoService.ProductProtoServiceClient(channel);
+
+                Console.WriteLine("AddProductAsync started..");
+
+                var addProductResponse = await client.AddProductAsync(
+                        new AddProductRequest
+                        {
+                            Product = new ProductModel
+                            {
+                                Name = productName + DateTimeOffset.Now,
+                                Description = "Product added from client",
+                                Price = 312,
+                                Status = ProductStatus.Instock,
+                                CreatedTime = Timestamp.FromDateTime(DateTime.UtcNow)
+                            }
+                        });
+
+                Console.WriteLine("AddProduct Response: " + addProductResponse.ToString());
 
                 await Task.Delay(taskInterval, stoppingToken);
             }
