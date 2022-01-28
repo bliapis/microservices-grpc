edd424feac5d6a3cd9951bef9bc409507091c9b6
Bruno Liapis <liapisb@gmail.com>
2022-01-28 13:15:53 -0300

    added discount Grpc microservice

---
+++ DiscountGrpc/Data/DiscountContext.cs
@@ -0,0 +1,15 @@
+﻿using DiscountGrpc.Model;
+using System.Collections.Generic;
+
+namespace DiscountGrpc.Data
+{
+    public class DiscountContext
+    {
+        public static readonly List<Discount> Discount = new List<Discount>
+        {
+            new Discount{DiscountId = 1, Code = "CDD100", Amount = 100 },
+            new Discount{DiscountId = 2, Code = "CDD200", Amount = 200 },
+            new Discount{DiscountId = 3, Code = "CDD300", Amount = 300 }
+        };
+    }
+}
---
+++ DiscountGrpc/Services/DiscountService.cs
@@ -0,0 +1,43 @@
+﻿using DiscountGrpc.Data;
+using DiscountGrpc.Protos;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscountGrpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(ILogger<DiscountService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override Task<DiscountModel> GetDiscount(GetDiscountRequest request,
+            ServerCallContext context)
+        {
+            var discount = DiscountContext.Discount.FirstOrDefault(d => d.Code == request.DiscountCode);
+
+            if (discount is null)
+            {
+                _logger.LogInformation($"Discount code {request.DiscountCode} does not exists.");
+
+                throw new RpcException(new Status(StatusCode.NotFound,
+                    $"Discount code {request.DiscountCode} does not exists."));
+            }
+
+            var discountModel = new DiscountModel()
+            {
+                DiscountId = discount.DiscountId,
+                Code = discount.Code,
+                Amount = discount.Amount
+            };
+
+            return Task.FromResult(discountModel);
+        }
+    }
+}
